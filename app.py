de6ad479744929e6678e5d42a48f51632d5e4307
Jason Tung <mickeu420@gmail.com>
2019-04-02 23:33:09 -0400

    sdjkalfjdlskf

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, render_template #stdlib
+import json
 import pymongo
+
 
 from os import urandom
 
@@ -8,18 +10,33 @@ import util.util_stuff.find as finder
 app = Flask(__name__)
 app.secret_key = urandom(32)
 
+import json
+from bson import ObjectId
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+
 @app.route('/test')
 def test():
     ip = "jayy.mooo.com"
     connection = pymongo.MongoClient(ip)
     connection.server_info()
     db = connection.test
-    all_pokemon = finder.find_pokemans(db)
-    results = []
-    for doc in all_pokemon:
-        results.append( jsonify(doc) )
-    print(results)
-    return render_template('index.html', list_all_pokemon = results)
+    cursor = finder.find_pokemans(db)
+    # for k in cursor:
+    #     print(k)
+    #     print(type(k))
+    all_pokemon = [k for k in cursor]
+    # print(all_pokemon)
+    # results = []
+    # for doc in all_pokemon:
+    #     results.append( str(doc) )
+    # print(results)
+    return render_template('index.html', list_all_pokemon = JSONEncoder().encode(all_pokemon))
 
 if __name__ == '__main__':
     app.debug = True #set to False in production mode
